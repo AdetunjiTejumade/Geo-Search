05e0a7d9529b88e3513d3e36a938dadc20c96a0e
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2019-11-28 12:47:00 +0100

    feature(postal code): add postal code feature

--- function.js
+++ function.js
@@ -1,13 +1,17 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 const place = document.getElementById('place');
-const mapCon = document.getElementById('mapArea');
+const mapCon = document.getElementById('map-img');
 const btn = document.getElementById('go');
+
 // const weatherBtn = document.getElementById('weather-btn');
 const weather = document.getElementById('weather');
 const temp = document.getElementById('temp');
-const humidity = document.getElementById('hum');
+const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
+const zipCode = document.getElementById('zipcode');
+const cityName = document.getElementById('city');
+
 // /apis
 // OpenWeather API
 
@@ -34,11 +38,15 @@ const windSpeed = document.getElementById('wind-speed');
 
 const getWeatherData = async () => {
   try {
-    // const b = await fetch(`https://maps.googleapis.com/maps/api/geocode/xml?address=${place.value}&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4`);
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place.value}&units=metric&APPID=7d62003427c83ed606f15ceb8edeb5c1`);
-    const data = response.json();
+    const [data, zip] = await Promise.all([
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place.value}&units=metric&APPID=7d62003427c83ed606f15ceb8edeb5c1`).then((response) => response.json()),
+      fetch(`https://api.opencagedata.com/geocode/v1/json?q=${place.value}&key=32b68c57341742378c21b593c79ca9fa`).then((response) => response.json()),
+    ]);
+
     // console.log(b.json());
-    return data;
+    // console.log(data);
+
+    return { data, zip };
   } catch (e) {
     console.log(e);
   }
@@ -46,22 +54,31 @@ const getWeatherData = async () => {
 // https://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
 
 const mapArea = (a, b) => {
-  mapCon.style.backgroundImage = 'url(' + 'https://maps.googleapis.com/maps/api/staticmap?center='+ a +','+ b +'&zoom=14&scale=1&size=600x300&maptype=hybrid&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7'+ a.value +')';
+  mapCon.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + a +','+ b +'&zoom=13&scale=1&size=600x300&maptype=hybrid&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:C%7C' + a +','+ b +'';
   // 'url(' + 'https://maps.googleapis.com/maps/api/staticmap?center=' + a +','+ b +',13/400x400?access_token=pk.eyJ1IjoidGVqdW1hZGUiLCJhIjoiY2sybWFwejFiMGVsZjNqbjV5NmdqdzZyZSJ9.lOgB0z3a000HaKHWx94IIQ' + ')'
 };// https://maps.googleapis.com/maps/api/staticmap?Lagos,+Nigeria&zoom=14&scale=1&size=600x300&maptype=hybrid&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7CLagos,+Nigeria
 const weatherReport = (data) => {
-  console.log(data.main.temp);
-  temp.innerHTML = ` Temp ${data.main.temp}`;
-  humidity.innerHtml = ` Humidity ${data.main.humidity}`;
-  // windSpeed.innerHTML = 'Wind-speed '+ data.
+  console.log(data[1].results[0].components.postcode);
+  temp.innerHTML = ` Temp - ${data[0].main.temp}`;
+  humidity.innerHTML = `Humidity - ${data[0].main.humidity}`;
+  windSpeed.innerHTML = ` Wind-speed - ${data[0].wind.speed}`;
+  zipCode.innerHTML = ` Postal-code - ${data[1].results[0].components.postcode}`;
+  cityName.innerHTML = `${data[1].results[0].components.city}`;
 };
 
 btn.addEventListener('click', () => {
-  getWeatherData()
-    .then((data) => {
+  getWeatherData();
+  const a = getWeatherData()
+
+  // const { data, zip } = a
+    .then((f) => {
+      const { data } = f;
+      const { zip } = f;
+      console.log(data);
+      console.log(zip);
       const mapLon = data.coord.lon;
       const mapLat = data.coord.lat;
-      const weatherData = data;
+      const weatherData = [data, zip];
       const geoCode = 'hello';
 
       console.log(geoCode);
