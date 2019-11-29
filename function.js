33dab950365da84fb647a85c344aae922a692bf6
Adetunji Tejumade <tejumadeadetunji@gmail.com>
2019-11-29 19:47:03 +0100

    feature(convert):add temperature converting feature

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-operators */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 const place = document.getElementById('place');
@@ -10,58 +11,86 @@ const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
 const zipCode = document.getElementById('zipcode');
 const cityName = document.getElementById('city');
-
+const convertTempBtn = document.getElementById('convert-temp');
+const share = document.getElementById('share');
+const timeZone = document.getElementById('time-zone');
+const pressure = document.getElementById('pressure');
+const condition = document.getElementById('condition');
+const cloud = document.getElementById('cloud-cover');
+const temperatureSpan = document.getElementById('tempSpan');
+const errorC = document.getElementById('error');
 
 const getWeatherData = async () => {
   try {
+    errorC.style.display = 'none';
     const [data, zip] = await Promise.all([
       fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place.value}&units=metric&APPID=7d62003427c83ed606f15ceb8edeb5c1`).then((response) => response.json()),
       fetch(`https://api.opencagedata.com/geocode/v1/json?q=${place.value}&key=32b68c57341742378c21b593c79ca9fa`).then((response) => response.json()),
     ]);
-
-    // console.log(b.json());
-    // console.log(data);
-
     return { data, zip };
+  } catch (e) {
+    errorC.style.display = 'inline';
+    errorC.innerHTML = 'LOCATION NOT FOUND  TRY AGAIN OR INPUT A VALID ADDRESS  ';
+    console.log(e);
+  }
+};
+
+const mapArea = (a, b) => {
+  try {
+    mapCon.style.opacity = '100';
+    mapCon.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + a + ',' + b + '&zoom=13&scale=1&size=600x300&maptype=hybrid&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:C%7C' + a + ',' + b + '';
   } catch (e) {
     console.log(e);
   }
 };
-// https://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
-
-const mapArea = (a, b) => {
-  mapCon.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + a +','+ b +'&zoom=13&scale=1&size=600x300&maptype=hybrid&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:C%7C' + a +','+ b +'';
-  // 'url(' + 'https://maps.googleapis.com/maps/api/staticmap?center=' + a +','+ b +',13/400x400?access_token=pk.eyJ1IjoidGVqdW1hZGUiLCJhIjoiY2sybWFwejFiMGVsZjNqbjV5NmdqdzZyZSJ9.lOgB0z3a000HaKHWx94IIQ' + ')'
-};// https://maps.googleapis.com/maps/api/staticmap?Lagos,+Nigeria&zoom=14&scale=1&size=600x300&maptype=hybrid&key=AIzaSyBsGZ2MDH8SfdeNXY1cZe49sClfsBx-0_4&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7CLagos,+Nigeria
 const weatherReport = (data) => {
-  console.log(data[1].results[0].components.postcode);
-  temp.innerHTML = ` Temp - ${data[0].main.temp}`;
-  humidity.innerHTML = `Humidity - ${data[0].main.humidity}`;
-  windSpeed.innerHTML = ` Wind-speed - ${data[0].wind.speed}`;
-  zipCode.innerHTML = ` Postal-code - ${data[1].results[0].components.postcode}`;
+  // console.log(data[0].weather[0].main);
+  temp.innerHTML = `${data[0].main.temp}`;
+  // temp.textContent = 23;
+  humidity.innerHTML = ` ${data[0].main.humidity}`;
+  windSpeed.innerHTML = `  ${data[0].wind.speed}`;
+  zipCode.innerHTML = ` ${data[1].results[0].components.postcode}`;
   cityName.innerHTML = `${data[1].results[0].components.city}`;
+  timeZone.innerHTML = `${data[1].results[0].annotations.timezone.short_name}`;
+  pressure.innerHTML = `${data[0].main.pressure}`;
+  condition.innerHTML = `${data[0].weather[0].description}`;
+  cloud.innerHTML = `${data[0].clouds.all}`;
 };
+
+
+convertTempBtn.addEventListener('click', () => {
+  const curTmp = parseInt(temp.textContent);
+  // console.log(curTmp);
+  const celcius = curTmp * 9 / 5 + 32;
+  if (temperatureSpan.textContent === 'C') {
+    // convert to fah
+    temperatureSpan.textContent = 'F';
+    temp.textContent = celcius;
+    convertTempBtn.innerHTML = 'Convert <sup>o</sup>F to<sup>o</sup>C';
+  } else {
+    const cel = curTmp;
+    temp.textContent = Math.round((cel - 32) * 5 / 9);
+    temperatureSpan.textContent = 'C';
+    convertTempBtn.innerHTML = 'Convert <sup>o</sup>C to<sup>o</sup>F';
+  }
+});
 
 btn.addEventListener('click', () => {
   getWeatherData();
   const a = getWeatherData()
-
-  // const { data, zip } = a
     .then((f) => {
       const { data } = f;
       const { zip } = f;
-      console.log(data);
-      console.log(zip);
       const mapLon = data.coord.lon;
       const mapLat = data.coord.lat;
       const weatherData = [data, zip];
-      const geoCode = 'hello';
-
-      console.log(geoCode);
       mapArea(mapLat, mapLon);
       weatherReport(weatherData);
     });
 });
+// temp.textContent = '23';
+// Temperature conversion
+
 
 const input = document.getElementById('place');
 const autocomplete = new google.maps.places.Autocomplete(input);
